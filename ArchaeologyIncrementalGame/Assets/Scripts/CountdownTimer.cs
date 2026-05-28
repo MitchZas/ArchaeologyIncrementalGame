8acafc170b8661de9af75bab09575761db03c8d1
MitchZas <mitchellzasa@gmail.com>
2026-05-28 10:02:51 -0400

    Added a 15 second countdown timer in the top right corner
    Still to do for the timer:
    - Change the color to yellow when between 6 - 10 seconds
    - Change the color to red when between 0 -5 seconds

---
+++ ArchaeologyIncrementalGame/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CountdownTimer : MonoBehaviour
+{
+    [SerializeField] private UIDocument uiDocument;
+    [SerializeField] private float startTime = 15f;
+
+    private Label _timerLabel;
+    private float _currentTime;
+    private bool isRunning = true;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _currentTime = startTime;
+        isRunning = true;
+        _timerLabel = uiDocument.rootVisualElement.Q<Label>("TimerLabel");
+        UpdateTimerDisplay();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isRunning) return;
+
+        _currentTime -= Time.deltaTime;
+        Debug.Log(_currentTime);
+
+        if (_currentTime <= 0)
+        {
+            _currentTime = 0;
+            isRunning = false;
+        }
+            
+        UpdateTimerDisplay();
+    }
+
+    private void UpdateTimerDisplay()
+    {
+        int seconds = Mathf.FloorToInt(_currentTime);
+        int milliseconds = Mathf.FloorToInt((_currentTime % 1) * 100);
+        _timerLabel.text = string.Format("{0:00}:{1:00}", seconds, milliseconds);
+    }
+}
